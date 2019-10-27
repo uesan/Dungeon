44555161099508637131223634c5819fdc45f7ae
uesan <kotaro0310ilikeblue@gmail.com>
2019-10-27 20:24:05 +0900

    This commit include to create a few chara's art and one script Actor.

---
+++ Assets/Script/Actor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Actor : MonoBehaviour
+{
+    private int health;
+    private int attack;
+    private int level;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
